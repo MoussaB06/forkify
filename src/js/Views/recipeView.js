bc2e117f4b2ba6c4491b54371da956d93b707495
MoussaB06 <mohamedmoussa.benmouhoub@se.univ-bejaia.dz>
2025-04-22 05:53:10 +0100

    Last commit

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,51 +1,21 @@
-import icons from 'url:../../img/icons.svg';
-import View from './View';
-
-// Fraction
-function formatFraction(value) {
-  if (!value) return '';
-  const tolerance = 1.0e-6;
-  let h1 = 1,
-    h2 = 0,
-    k1 = 0,
-    k2 = 1,
-    b = value;
-  do {
-    const a = Math.floor(b);
-    let aux = h1;
-    h1 = a * h1 + h2;
-    h2 = aux;
-    aux = k1;
-    k1 = a * k1 + k2;
-    k2 = aux;
-    b = 1 / (b - a);
-  } while (Math.abs(value - h1 / k1) > value * tolerance);
-
-  const numerator = h1;
-  const denominator = k1;
-
-  const whole = Math.floor(numerator / denominator);
-  const remainder = numerator % denominator;
-
-  if (whole === 0) return `${remainder}/${denominator}`;
-  if (remainder === 0) return `${whole}`;
-  return `${whole} ${remainder}/${denominator}`;
-}
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2 compatible
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
-  _data;
-  _errorMessage = 'We could not find that recipe. Please try another one';
+  _errorMessage = 'We could not find that recipe. Please try another one!';
   _message = '';
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
 
   addHandlerUpdateServings(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--update-servings');
       if (!btn) return;
-
-      // 'update-to' sera reconvertis en 'updateTo'
-      const updateTo = +btn.dataset.updateTo;
-      if (updateTo > 0) handler(updateTo);
+      const { updateTo } = btn.dataset;
+      if (+updateTo > 0) handler(+updateTo);
     });
   }
 
@@ -59,107 +29,124 @@ class RecipeView extends View {
 
   _generateMarkup() {
     return `
-        <figure class="recipe__fig">
-          <img src="${this._data.image}" alt="${
+      <figure class="recipe__fig">
+        <img src="${this._data.image}" alt="${
       this._data.title
     }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${this._data.title}</span>
-          </h1>
-        </figure>
+        <h1 class="recipe__title">
+          <span>${this._data.title}</span>
+        </h1>
+      </figure>
 
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              this._data.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              this._data.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            this._data.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${
+            this._data.servings
+          }</span>
+          <span class="recipe__info-text">servings</span>
 
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--update-servings" data-update-to="${
-                this._data.servings - 1
-              }">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--update-servings" data-update-to="${
-                this._data.servings + 1
-              }">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings - 1
+            }">
+              <svg><use href="${icons}#icon-minus-circle"></use></svg>
+            </button>
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings + 1
+            }">
+              <svg><use href="${icons}#icon-plus-circle"></use></svg>
+            </button>
           </div>
-
-          <div class="recipe__user-generated">
-            
-          </div>
-          <button class="btn--round btn--bookmark">
-            <svg class="">
-              <use href="${icons}#icon-bookmark${
-      this._data.bookMarked === true ? '-fill' : ''
-    }"></use>
-            </svg>
-          </button>
         </div>
 
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-            ${this._data.ingredients
-              .map(
-                ing => `<li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${formatFraction(
-                ing.quantity
-              )}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>`
-              )
-              .join('')}
-          </ul>
+        <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+          <svg><use href="${icons}#icon-user"></use></svg>
         </div>
+        <button class="btn--round btn--bookmark">
+          <svg class=""><use href="${icons}#icon-bookmark${
+      this._data.bookMarked ? '-fill' : ''
+    }"></use></svg>
+        </button>
+      </div>
 
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              this._data.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${this._data.sourceURL}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </a>
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${this._data.ingredients
+            .map(this._generateMarkupIngredient.bind(this))
+            .join('')}
+        </ul>
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${
+            this._data.publisher
+          }</span>. Please check out directions at their website.
+        </p>
+        <a class="btn--small recipe__btn" href="${
+          this._data.sourceUrl
+        }" target="_blank">
+          <span>Directions</span>
+          <svg class="search__icon"><use href="${icons}#icon-arrow-right"></use></svg>
+        </a>
+      </div>
+    `;
+  }
+
+  _generateMarkupIngredient(ing) {
+    return `
+      <li class="recipe__ingredient">
+        <svg class="recipe__icon"><use href="${icons}#icon-check"></use></svg>
+        <div class="recipe__quantity">${
+          ing.quantity ? this._formatFraction(ing.quantity) : ''
+        }</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${ing.unit}</span>
+          ${ing.description}
         </div>
-        `;
+      </li>
+    `;
+  }
+
+  _formatFraction(value) {
+    const tolerance = 1.0e-6;
+    let h1 = 1,
+      h2 = 0,
+      k1 = 0,
+      k2 = 1,
+      b = value;
+
+    for (let i = 0; i < 100; i++) {
+      if (Math.abs(value - h1 / k1) < tolerance) break;
+      const a = Math.floor(b);
+      [h1, h2] = [a * h1 + h2, h1];
+      [k1, k2] = [a * k1 + k2, k1];
+      b = 1 / (b - a);
+    }
+
+    const numerator = h1;
+    const denominator = k1;
+    const whole = Math.floor(numerator / denominator);
+    const remainder = numerator % denominator;
+
+    if (whole === 0) return `${remainder}/${denominator}`;
+    if (remainder === 0) return `${whole}`;
+    return `${whole} ${remainder}/${denominator}`;
   }
 }
 
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,44 +1,37 @@
 import * as model from './model.js';
-import recipeView from './Views/recipeView.js';
-import searchView from './Views/searchView.js';
-import resultsView from './Views/resultsView.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import paginationView from './Views/paginationView.js';
+import { async } from 'regenerator-runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
-// const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
-// Declarations des Fonctions
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
 
-    /// Loading the recipe
-    // La recette loader vas etre stocké dans l'objet state dans model
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    // Rendering the recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
-
-    /*
-    // Apres avoir chargé la recette on modifie ces ingredients avec ça !
-    controlServings();
-    */
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -46,66 +39,93 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
 
-    /// 1 Get search Query
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
-    /// 2 Load search results
-    // On ne la stock pas dans une const car elle ne retourne rien
+    // 2) Load search results
     await model.loadSearchResults(query);
 
-    /// 3 Render results
-    // resultsView.render(model.state.search.results);
+    // 3) Render results
     resultsView.render(model.getSearchResultsPage());
 
-    /// 4 Render initials pagination button
+    // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 };
 
 const controlPagination = function (goToPage) {
+  // 1) Render NEW results
   resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
   paginationView.render(model.state.search);
 };
 
 const controlServings = function (newServings) {
-  /// Update the recipe servings (in state)
+  // Update the recipe servings (in state)
   model.updateServings(newServings);
 
-  /// Update the recipe view
-  // recipeView.render(model.state.recipe);
+  // Update the recipe view
   recipeView.update(model.state.recipe);
 };
 
-// const controlAddBookMark = function () {
-//   if (!model.state.recipe.bookMarked) model.addBookMark(model.state.recipe);
-//   if (model.state.recipe.bookMarked)
-//     model.deleteBookMark(model.state.recipe.id);
-//   recipeView.update(model.state.recipe);
-// };
+const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
 
-const controlAddBookMark = function () {
-  if (!model.state.recipe.bookMarked) {
-    model.addBookMark(model.state.recipe);
-  } else {
-    model.deleteBookMark(model.state.recipe.id);
-  }
-
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
-  recipeView.addHandlerAddBookmark(controlAddBookMark);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
-
-  // controlServings();
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
-// if (model.state.recipe.ingredients) controlServings();
-
-// Appel des Fonctions
 init();
